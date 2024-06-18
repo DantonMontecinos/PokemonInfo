d546e901620f039426150b588282030e6c6536ce
DantonMontecinos <95037951+DantonMontecinos@users.noreply.github.com>
2024-06-18 14:44:23 -0300

    Proyecto para el coloquio finalizado

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('.carousel').carousel({
+        interval: 3000, // Intervalo de tiempo en milisegundos entre slides
+        pause: 'hover' // Pausa el carrusel en hover
+    });
+});
